030a6075f2d23470d5009448fc1fd56800bfab52
TRAMB1E <arsenuk.andrey@gmail.com>
2025-12-03 16:40:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("App loaded");
+// Переключение вкладок
+document.querySelectorAll(".tab").forEach(tab => {
+  tab.addEventListener("click", () => {
+    document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
+    tab.classList.add("active");
+  });
+});
+
+// Чекбоксы
+document.querySelectorAll(".checkbox").forEach(box => {
+  box.addEventListener("click", () => {
+    box.classList.toggle("checked");
+  });
+});
